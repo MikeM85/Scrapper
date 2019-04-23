685893a26af979e7c2ba59760dbb2d7c78cc3afc
Mike <mikeinhistory@gmail.com>
2019-04-23 16:01:15 -0500

    Scraping title and link, need to connect to Dto db and display

--- server.js
+++ server.js
@@ -22,24 +22,27 @@ axios.get("https://www.reddit.com/r/PlantedTank/").then(function(response) {
   // With cheerio, find each p-tag with the "title" class
   // (i: iterator. element: the current element)
   $("span.y8HYJ-y_lTUHkQIc1mdCq").each(function(i, element) {
-
+   
     // Save the text of the element in a "title" variable
     var title = $(element).text();
 
     // In the currently selected element, look at its child elements (i.e., its a-tags),
     // then save the values for any "href" attributes that the child elements may have
-    var link = $(element).children().attr("a.body");
+    var link = $(element).children().attr("href");
+
+    // var image = $("div.m3aNC6yp8RrNM_-a0rrfa").children().attr("img");
 
     // Save these results in an object that we'll push into the results array we defined earlier
     results.push({
       title: title,
-      link: link
+      link: link,
+      // image: image
     });
   });
 
+
   // Log the results once you've looped through each of the elements found with cheerio
   console.log(results);
+// });
+
 });
-
-
-// span.y8HYJ-y_lTUHkQIc1mdCq
